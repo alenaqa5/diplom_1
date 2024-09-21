18ba5334be22bc1a8a9a738be7fea3893e0e1783
AlenaQa5 <alyonagraun@gmail.com>
2024-09-21 11:40:06 +0200

    Добавлены тесты на модули ingredient и ingredient_type, в файл с данными добавлены данные для тестов по ингредиентам, добавлена фикстура в conftest.py для создания ингредиентов. переименованы тесты для булочек.

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,6 +1,6 @@
 class Testbun:
-    def test_add_bun_name(self, create_bun):
+    def test_get_bun_name(self, create_bun):
         assert create_bun.get_name() == 'Кунжутная'
 
-    def test_add_bun_price(self, create_bun):
+    def test_get_bun_price(self, create_bun):
         assert create_bun.get_price() == 100
